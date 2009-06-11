9b2adad5ae28bd10fd00b4c65c59d081fcefee18
laurent.foret <laurent.foret@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-06-11 11:26:08 +0000

    introduce step4

--- trunk/20090611-maven/helloWorldApp/src/main/java/org/bordeauxjug/hello/UnauthorizedApp.java
+++ trunk/20090611-maven/helloWorldApp/src/main/java/org/bordeauxjug/hello/UnauthorizedApp.java
@@ -2,6 +2,14 @@ package org.bordeauxjug.hello;
 
 //import org.apache.log4j.Logger;
 
+
+/**
+ * This application say "Hello World !" using the {@link org.apache.log4j.Logger} class.
+ * Should not existed.
+ * 
+ * @since 1.0
+ * @author lfo
+ */
 public class UnauthorizedApp {
 
 //    private static Logger log4jLogger = Logger.getLogger(App.class);
