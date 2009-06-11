9c75872b74101e9ba85aa330c928bd6e7acc74be
laurent.foret <laurent.foret@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-06-11 11:26:08 +0000

    introduce step4

--- 20090611-maven/helloWorldApp/src/test/java/org/bordeauxjug/hello/AppTest.java
+++ 20090611-maven/helloWorldApp/src/test/java/org/bordeauxjug/hello/AppTest.java
@@ -4,10 +4,20 @@ import junit.framework.TestCase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+
+/**
+ * The test for the application launching.
+ * 
+ * @author lfo
+ */
 public class AppTest extends TestCase {
 
     private static Log commonsLogger = LogFactory.getLog(AppTest.class);
 
+
+    /**
+     * Simple Test
+     */
     public void testApp() {
         commonsLogger.debug("testApp is called !");
         App.main(new String[]{});
