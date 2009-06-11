603612d2063df028d1c121cc5361729e6b91225e
laurent.foret <laurent.foret@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-06-11 11:28:57 +0000

    add step4

---
+++ step4/helloWorldApp/src/main/java/org/bordeauxjug/hello/UnauthorizedApp.java
@@ -0,0 +1,20 @@
+package org.bordeauxjug.hello;
+
+//import org.apache.log4j.Logger;
+
+
+/**
+ * This application say "Hello World !" using the {@link org.apache.log4j.Logger} class.
+ * Should not existed.
+ * 
+ * @since 1.0
+ * @author lfo
+ */
+public class UnauthorizedApp {
+
+//    private static Logger log4jLogger = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+//        log4jLogger.info("Hello World!");
+    }
+}
