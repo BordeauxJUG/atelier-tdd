0157e3c190ba5c0abf7d207f95e2b9d476c98b31
fcamblor <fcamblor@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-11-14 17:17:12 +0000

    Predators no longer has Sensors ... IMoveBehaviourWithSensor has

--- trunk/20091105 - mocks/src/org/jug/tdd/model/IPredator.java
+++ trunk/20091105 - mocks/src/org/jug/tdd/model/IPredator.java
@@ -2,7 +2,7 @@ package org.jug.tdd.model;
 
 
 
-public interface IPredator extends IActor, ISensorable, IFoodModable, IMovable {
+public interface IPredator extends IActor, IFoodModable, IMovable {
 
 	void eat(IPrey prey);
 	
