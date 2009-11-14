77a2a5fdb92ba3815db76e7aebf383610ada3868
fcamblor <fcamblor@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-11-14 16:03:39 +0000

    added foodMode setter in IPredator + delegate method canEat() (in order to be able to inject IFoodMode mocks)

--- trunk/20091105 - mocks/src/org/jug/tdd/model/IPredator.java
+++ trunk/20091105 - mocks/src/org/jug/tdd/model/IPredator.java
@@ -1,7 +1,12 @@
 package org.jug.tdd.model;
 
 
+
 public interface IPredator extends IActor {
 
 	void eat(IPrey prey);
+	
+	boolean canEat(IPrey prey);
+	
+	void setFoodMode(IFoodMode foodMode);
 }
