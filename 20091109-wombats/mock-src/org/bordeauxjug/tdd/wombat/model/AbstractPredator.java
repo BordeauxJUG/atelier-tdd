ed14b1fce25d3c93dd3019313c69649db8d43a3d
fcamblor <fcamblor@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-11-15 19:39:15 +0000

    removed abstractactor (and subclasses) implementations

--- 20091109-wombats/mock-src/org/bordeauxjug/tdd/wombat/model/AbstractPredator.java
+++
@@ -1,8 +0,0 @@
-package org.bordeauxjug.tdd.wombat.model;
-
-public abstract class AbstractPredator extends AbstractActor implements IPredator {
-
-	protected AbstractPredator(String _name){
-		super(_name);
-	}
-}
--- 20091109-wombats/mock-src/org/bordeauxjug/tdd/wombat/model/AbstractPrey.java
+++
@@ -1,8 +0,0 @@
-package org.bordeauxjug.tdd.wombat.model;
-
-public abstract class AbstractPrey extends AbstractActor implements IPrey {
-
-	protected AbstractPrey(String _name){
-		super(_name);
-	}
-}
