b00f49532b59fb18e277bbafed4ac9f79ed3b11c
fcamblor <fcamblor@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-12-06 19:38:04 +0000

    added specific images for actors

--- trunk/20091109-wombats/mock-exemple-impl/org/bordeauxjug/tdd/wombat/model/Leaf2.java
+++ trunk/20091109-wombats/mock-exemple-impl/org/bordeauxjug/tdd/wombat/model/Leaf2.java
@@ -1,6 +1,10 @@
 package org.bordeauxjug.tdd.wombat.model;
 
+import greenfoot.GreenfootImage;
+
 public class Leaf2 extends AbstractPrey {
+
+	protected static final GreenfootImage LEAF_IMAGE = new GreenfootImage(IMAGES_ROOT_PATH+"leaf.gif");
 
 	public Leaf2(String _name){
 		super(_name, Kingdom.VEGETAL);
@@ -8,5 +12,14 @@ public class Leaf2 extends AbstractPrey {
 	
 	public Leaf2(String _name, Coordinates _coord){
 		super(_name, _coord, Kingdom.VEGETAL);
+	}
+	
+	@Override
+	public GreenfootImage getImage() {
+		if(isAlive()){
+			return LEAF_IMAGE;
+		} else {
+			return null;
+		}
 	}
 }
