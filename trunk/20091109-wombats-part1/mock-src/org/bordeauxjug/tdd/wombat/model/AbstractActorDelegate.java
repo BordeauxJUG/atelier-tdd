2819bec90dd740c4bbabf1e1e0a54a7d206f4ffa
fcamblor <fcamblor@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-12-07 21:21:21 +0000

    duplication vers la partie 1

---
+++ trunk/20091109-wombats-part1/mock-src/org/bordeauxjug/tdd/wombat/model/AbstractActorDelegate.java
@@ -0,0 +1,45 @@
+package org.bordeauxjug.tdd.wombat.model;
+
+import greenfoot.GreenfootImage;
+
+public abstract class AbstractActorDelegate implements IActorDelegate{
+
+	protected static final String IMAGES_ROOT_PATH = "C:/Users/Fred/workspaces/JUG/20091109-wombats/images/";
+	
+	protected static final GreenfootImage GREENFOOT_DEFAULT_IMAGE = new GreenfootImage(IMAGES_ROOT_PATH+"greenfoot.png");
+	
+	private String name;
+	private Coordinates coords;
+	private ActorDelegator<? extends IActorDelegate> greenfootActor;
+	
+	protected AbstractActorDelegate(String _name){
+		this(_name, new Coordinates(0, 0));
+	}
+	
+	protected AbstractActorDelegate(String _name, Coordinates _coord){
+		this.name = _name;
+		this.coords = _coord;
+		this.greenfootActor = null;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+
+	public Coordinates getCoordinates(){
+		return this.coords;
+	}
+
+	public void setGreenfootActor(ActorDelegator<? extends IActorDelegate> _greenfootActor){
+		this.greenfootActor = _greenfootActor;
+	}
+
+	public ActorDelegator<? extends IActorDelegate> getGreenfootActor() {
+		return greenfootActor;
+	}
+	
+	@Override
+	public GreenfootImage getImage() {
+		return GREENFOOT_DEFAULT_IMAGE;
+	}
+}
