e0898b45e115fca323dc613e3c521d5aa1a32f38
fcamblor <fcamblor@3afb6514-1970-11de-b56b-c9554f25afe1>
2009-12-07 22:11:49 +0000

    fix imports

--- trunk/20091109-wombats-part1/test/org/bordeauxjug/tdd/wombat/WombatWorldTest.java
+++ trunk/20091109-wombats-part1/test/org/bordeauxjug/tdd/wombat/WombatWorldTest.java
@@ -1,14 +1,10 @@
 package org.bordeauxjug.tdd.wombat;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import greenfoot.World;
 import greenfoot.util.GreenfootUtil;
 
 import org.bordeauxjug.tdd.wombat.greenfoot.TestUtilDelegate;
 import org.bordeauxjug.tdd.wombat.greenfoot.WorldCreator;
-import org.bordeauxjug.tdd.wombat.model.Leaf;
-import org.bordeauxjug.tdd.wombat.model.Wombat;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
